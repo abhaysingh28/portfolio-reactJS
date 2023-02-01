d411656e79baa8430237f02c481f792f8bfc63e1
अभय सिंह <99662885+abhaysingh28@users.noreply.github.com>
2023-02-01 16:50:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './Components/Home/Home';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
